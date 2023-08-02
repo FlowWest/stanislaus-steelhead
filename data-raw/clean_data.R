924d49863db623c387e935fdf4c97f581c1d4944
liz-fw <lstebbins@flowwest.com>
2023-08-02 09:07:43 -0700

    fixes dashes in carcass and redd data

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,22 +1,18 @@
 library(tidyverse)
 
 # questions
-# TODO do we want "-" as an option for all the y/n columns? (carcass & redd)
 # TODO 172 rows in steelhead adult/juvenile that have NA date
-# TODO units for distance in redd data?
-# TODO check units for environmental data
-# TODO check coverage, project metadata
 
 
 # carcass
 carcass <- read.csv(here::here("data-raw", "steelhead-carcass-data-2021-2022.csv")) |>
   mutate(date = as.Date(date, format = "%m/%d/%Y"),
          notes = ifelse(notes == "", NA_character_, notes),
-         adclip = ifelse(adclip == "-", NA_character_, adclip),
-         scales = ifelse(scales == "-", NA_character_, scales),
-         head = ifelse(head == "-", NA_character_, head),
-         otoliths = ifelse(otoliths == "-", NA_character_, otoliths),
-         fin_clip = ifelse(fin_clip == "-", NA_character_, fin_clip)) |> glimpse()
+         adclip = ifelse(adclip %in% c("-", NA), "no sample", adclip),
+         scales = ifelse(scales  %in% c("-", NA), "no sample", scales),
+         head = ifelse(head  %in% c("-", NA), "no sample", head),
+         otoliths = ifelse(otoliths %in% c("-", NA), "no sample", otoliths),
+         fin_clip = ifelse(fin_clip  %in% c("-", NA), "no sample", fin_clip)) |> glimpse()
 
 # redd
 redd <- read.csv(here::here("data-raw", "steelhead-and-lamprey-redd-data-2021-2022.csv")) |>
@@ -25,11 +21,11 @@ redd <- read.csv(here::here("data-raw", "steelhead-and-lamprey-redd-data-2021-20
          pot_width_cm = ifelse(pot_width_cm == "", NA, pot_width_cm),
          pot_depth_cm = ifelse(pot_depth_cm == "", NA, pot_depth_cm),
          est_pot_substrate = ifelse(est_pot_substrate %in% c(" -", ""), NA, est_pot_substrate),
-         est_tailspin_substrate = ifelse(est_tailspin_substrate == "", NA, est_tailspin_substrate),
-         est_ambient_substrate = ifelse(est_ambient_substrate == "", NA, est_ambient_substrate),
+         est_tailspin_substrate = ifelse(est_tailspin_substrate %in% c("", " -"), NA, est_tailspin_substrate),
+         est_ambient_substrate = ifelse(est_ambient_substrate %in% c("", " -"), NA, est_ambient_substrate),
          X = ifelse(X == "", NA, X),
-         ambient_velocity_ms = ifelse(ambient_velocity_ms == "", NA, ambient_velocity_ms),
-         average_velocity = ifelse(average_velocity == "", NA, average_velocity),
+         ambient_velocity_ms = ifelse(ambient_velocity_ms %in% c("", " -", "-"), NA, ambient_velocity_ms),
+         average_velocity = ifelse(average_velocity %in% c("", " -", "-"), NA, average_velocity),
          fish_present = ifelse(fish_present == "", NA, fish_present),
          fish_1_sex = ifelse(fish_1_sex %in% c("", " -"), NA, fish_1_sex),
          fish_1_size = ifelse(fish_1_size %in% c("", " -"), NA, fish_1_size),
